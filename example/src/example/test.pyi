6176b53d298c680af157ab00e08c3036eff861f0
benefactarch <auch.josh@gmail.com>
2023-08-25 01:15:17 -0400

    refactor: pyi & release [ci skip]

--- example/src/example/test.pyi
+++ example/src/example/test.pyi
@@ -1,1 +1,10 @@
+from typing import Generic, Protocol, TypeVar
+
+from numpy import longlong
+
+T = TypeVar("T")
+
+class NDArray(Generic[T], Protocol): ...
+
 def hello_world(name: str): ...
+def hello_numpy(arr: NDArray[longlong]): ...
