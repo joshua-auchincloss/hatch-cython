3122709b34d85ea60d272c4fa94c9420028d4e6c
benefactarch <auch.josh@gmail.com>
2023-08-26 18:33:25 -0400

    fix: coverage

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,18 @@
 # SPDX-FileCopyrightText: 2023-present joshua-auchincloss <joshua.auchincloss@proton.me>
 #
 # SPDX-License-Identifier: MIT
+from os import getcwd, name
+from sys import path
+
+if name == "nt":
+    from pathlib import WindowsPath
+
+    p = WindowsPath(getcwd()) / "src"
+else:
+    from pathlib import PosixPath
+
+    p = PosixPath(getcwd()) / "src"
+
+path.append(str(p))
+
+from hatch_cython.__about__ import __version__  # noqa: E402
