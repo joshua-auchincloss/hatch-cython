e4fa59673acc3ec0ba39390af0e12d5672c36ff2
benefactarch <auch.josh@gmail.com>
2023-08-28 02:39:07 -0400

    fix: sorting error

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -84,7 +84,7 @@ def test_build_hook(new_proj):
             ]
         )
 
-        assert hook.grouped_included_files == [
+        assert [sorted(ls) for ls in hook.grouped_included_files] == [sorted(ls) for ls in [
             ["./src/example_lib/normal.py"],
             ["./src/example_lib/__init__.py"],
             ["./src/example_lib/__about__.py"],
@@ -92,7 +92,7 @@ def test_build_hook(new_proj):
             ["./src/example_lib/mod_a/some_defn.py", "./src/example_lib/mod_a/some_defn.py"],
             ["./src/example_lib/test.pyx"],
             ["./src/example_lib/mod_a/adds.pyx"],
-        ]
+        ]]
 
         rf = sorted(
             [
