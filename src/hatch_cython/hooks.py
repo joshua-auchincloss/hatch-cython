4f33c51ef86b6453b874505aaa4397a546c2efbf
Clemens Kubach <clemens.kubach@gmail.com>
2024-08-11 18:25:49 +0200

    test hacky patched WheelBuilder

--- src/hatch_cython/hooks.py
+++ src/hatch_cython/hooks.py
@@ -13,16 +13,26 @@ def hatch_register_build_hook():
 @hookimpl
 def hatch_register_builder():
     from hatchling.builders import wheel as wheel
+    print("test1")
 
     wheel.DefaultWheelBuilder = wheel.WheelBuilder
 
     class WheelBuilder(wheel.WheelBuilder):
-        PLUGIN_NAME = "hatch_cython"
         # TODO allow any custom modifications
         # TODO overwrite get_default_build_data(), set_build_data_defaults(), config.set_build_data()=WheelBuilderConfig
+        def __init__(
+                self,
+                root: str,
+                plugin_manager = None,
+                config = None,
+                metadata = None,
+                app = None,
+        ) -> None:
+            super().__init__(root, plugin_manager, config, metadata, app)
+            print("test2")
 
         def build_standard(self, directory: str, **build_data: Any) -> str:
-            print("WHEEL BUILDER OVERRIDE")
+            print("test3")
             return super().build_standard(directory, **build_data)
 
     wheel.WheelBuilder = WheelBuilder
